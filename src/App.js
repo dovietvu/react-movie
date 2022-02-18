636c40811258df4e4b4a4ab7f0f70e3bf9862901
vuxdvi <vietvua12@gmail.com>
2022-02-18 12:58:24 +0700

    publish router

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import "slick-carousel/slick/slick-theme.css";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Header />
         <div className='container'>
           <Switch>
